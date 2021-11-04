523691c75d4868fef2a8a69b9093a9615e0377b8
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2021-11-04 13:26:01 -0300

    iniciado carro

---
+++ 008_carro/carro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+class CARRO {
+    int gas;
+    int maxGas;
+    int km;
+    std::vector<int> passageiro;
+    int passMax;
+public:
+    CARRO(int gas, std::vector<int> passageiro, int km) : gas{0}, passageiro{}, {
+    }
+};
+
+int main(){
+    
+    
+    return 0;
+}
