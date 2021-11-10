1187f5d3e329811730a44a65799c58ea19b06558
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2021-11-10 11:12:08 -0300

    up

--- 010_pula_pula/pulapula.cpp
+++ 010_pula_pula/pulapula.cpp
@@ -5,40 +5,52 @@
 class CRIANCA{
     int idade;
     std::string nome;
-    int saldo;
+    int saldo {};
 
     public:
+        CRIANCA(std::string nome = "", int idade = 0) : nome{nome}, idade{idade} {};
+        
         // ADICIONA OS DADOS DA CRIANCA;
-        void setIdade(int idade){ this->idade = idade; }
-        void setNome(std::string nome){ this->nome = nome; }
+        //void setIdade(int idade){ this->idade = idade; }
+        //void setNome(std::string nome){ this->nome = nome; }
         void setSaldo(int saldo){ this->saldo = saldo; }
 
         // RETORNA OS DADOS DA CRIANCA
-        int getIdade(){ return this->idade; }
-        std::string getNome(){ return this->nome; }
+        //int getIdade(){ return this->idade; }
+        //std::string getNome(){ return this->nome; }
         int getSaldo(){ return this->saldo; }
+
+        friend std::ostream& operator<<(std::ostream& os, const CRIANCA& c) {
+            os << c.nome << ":" << c.idade << " ";
+            return os;
+        }
 };
 
 class PULA_PULA{
-    CRIANCA brincando;
+    CRIANCA* brincando;
     std::vector<CRIANCA> fila;
-    int caixa { 0 };
-    int max { 0 };
+    int caixa {};
+    int max {};
 
     public:
+        PULA_PULA(CRIANCA* brincando = nullptr) : brincando{brincando} {};
+
         void arrive(std::string nome, int idade){
-            CRIANCA aux;
-            aux.setNome(nome);
-            aux.setIdade(idade);
+            CRIANCA aux(nome, idade);
             fila.push_back(aux);
         }
 
         void show(){
             std::cout << "=> ";
-            for (int i = 0; i < (int)fila.size(); i++) {
-                std::cout << fila[i].getNome() << ":" << fila[i].getIdade() << " ";
-            }
-            std::cout << "=> " << " [ " << this->brincando.getNome() << ":" << this->brincando.getIdade() << " ]" << std::endl;
+            for (int i = 0; i < (int)fila.size(); i++)
+                std::cout << fila[i];
+            std::cout << "=> ";
+            if (brincando == nullptr) { std::cout << "[ ]" << std::endl; }
+            else { std::cout << "[ " << this->brincando << std::endl; }
+        }
+
+        void in(){
+            
         }
 };
 
@@ -57,14 +69,18 @@ int main(){
             break;
         }
         else if (cmd == "arrive") {
-            std::string nome;
-            int idade { 0 };
+            std::string nome {};
+            int idade {};
             ss >> nome >> idade;
             sistema.arrive(nome, idade);
         }
         else if (cmd == "show") {
             sistema.show();
         }
+        else if (cmd == "in") {
+            sistema.in();
+        }
     }
+
     return 0;
 }
